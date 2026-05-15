015f6714fd8f416c7f3569a372333ed72d566a7e
kwadjoWusu <wkjwusu@gmail.com>
2026-05-15 11:04:54 +0000

    refactor: add general errors to global errors

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,4 +1,23 @@
 export const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+  error.message = err.message;
+
+  console.error(error);
+
+  // Mongoose Bad ObjectID (CastError)
+  if (err.name === "CastError") {
+    error.message = "Invalid Task ID format";
+    error.statusCode = 400;
+  }
+
+  // Mongoose Validation Error
+  if (err.name === "ValidationError") {
+    error.message = Object.values(err.errors)
+      .map((val) => val.message)
+      .join(", ");
+    error.statusCode = 400;
+  }
+
   const statusCode = err.status || 500;
   res.status(statusCode).json({
     success: false,
